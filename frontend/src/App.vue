887ad1411ac62f503e724b0008d0e8046341790c
Mateus <matesantos@hotmail.com>
2020-05-28 21:49:39 -0300

    adição o do vue store.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <div id="app">
-      <Header />
+  <div id="app" :class="{'hide-menu': !isMenuVisible}">
+      <Header title="Cod3r - Projeto Final" :hideToggle="false"/>
       <Menu />
-      <Content />
       <Footer />
+      <Content />
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import Header from './components/templates/Header'
 import Menu from './components/templates/Menu'
 import Content from './components/templates/Content'
@@ -16,7 +17,8 @@ import Footer from '@/components/templates/Footer'
 
 export default {
     name:'App',
-    components: { Header, Menu, Content, Footer }
+    components: { Header, Menu, Content, Footer },
+    computed: mapState(['isMenuVisible'])
 }
 </script>
 
@@ -40,6 +42,11 @@ body{
     grid-template-areas:  "header header"
                           "menu content"
                           "menu footer";
-    gap: 3px;
+}
+
+#app.hide-menu {
+    grid-template-areas:  "header header",
+                          "content content",
+                          "footer footer";
 }
 </style>
--- frontend/src/components/templates/Header.vue
+++ frontend/src/components/templates/Header.vue
@@ -1,10 +1,31 @@
 <template>
-    <header class="header">header</header>
+    <header class="header">
+        <a class="toogle" @click="toggleMenu" v-if="!hideToggle">
+            <i class="fa lg" :class="icon"></i>
+        </a>
+        <h1 class="title">
+            {{ title }}
+        </h1>
+    </header>
 </template>
 
 <script>
 export default {
-    name: 'Header'
+    name: 'Header',
+    props: {
+        title: String,
+        hideToggle: Boolean
+    },
+    computed: {
+        icon(){
+            return this.$store.state.isMenuVisible ? "fa-angle-left" : "fa-angle-down"
+        }
+    },
+    methods:{
+        toggleMenu(){
+            this.$store.commit('toggleMenu')
+        }
+    }
 }
 </script>
 
@@ -16,5 +37,34 @@ export default {
     display: flex;
     justify-content: center;       
     align-content: center;
+}
+
+.title {
+    font-size: 1.2em;
+    color: #FFF;
+    font-weight: 100;
+    flex-grow: 1;
+    text-align: center;
+}
+
+.title a {
+    color: #FFF;
+    text-decoration: none;
+}
+
+header.header > a.toogle {
+    width: 60px;
+    height: 100%;
+    color: #FFF;
+    justify-content: flex-start;
+    text-decoration: none;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+header.header > a.toogle:hover {
+    background-color: rgba(0, 0, 0, 0.2);
 }
 </style>
